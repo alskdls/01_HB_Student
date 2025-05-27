790bd7e6653ed1036f1596dadc15bc2ceca153e0
WellDone <mdrebot2022@itcollege.lviv.ua>
2025-05-27 19:39:33 +0300

    add Docker

--- src/main/java/com/anish/App.java
+++ src/main/java/com/anish/App.java
@@ -5,6 +5,9 @@ import com.anish.dao.VykladachDao;
 
 public class App {
     public static void main(String[] args) {
+
+        System.out.println("Работает!");
+
         Vykladach vykladach = new Vykladach();
         vykladach.setId(1);
         vykladach.setLocation("Fizyka");
